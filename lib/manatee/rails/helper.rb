5f22190c68cb056ced8684a82d5ba65242b8eee7
Dalton <dalthon@dlt.local>
2015-04-13 11:48:53 -0300

    Small gambiarra fix

--- lib/manatee/rails/helper.rb
+++ lib/manatee/rails/helper.rb
@@ -2,7 +2,7 @@ module Manatee
   module Rails
     module Helper
       def csrf_meta_tags
-        (super + content_tag(:script, "Renderer.csrfToken = #{form_authenticity_token.inspect};".html_safe))
+        (super + content_tag(:script, "this.Renderer || (this.Renderer = {}); Renderer.csrfToken = #{form_authenticity_token.inspect};".html_safe))
       end
     end
   end
