908c70d2ba9ae12fb5abbf4b57a9d2040916daeb
Felipe JAPM <felipe.japm@gmail.com>
2015-09-16 18:33:08 -0300

    This monkey_patch is dangerous
    When testing protect_against_forgery is false, therefore no method is called
    hence this generate a undef method '+'

--- lib/manatee/rails/helper.rb
+++ lib/manatee/rails/helper.rb
@@ -2,7 +2,9 @@ module Manatee
   module Rails
     module Helper
       def csrf_meta_tags
-        (super + content_tag(:script, "this.Renderer || (this.Renderer = {}); Renderer.csrfToken = #{form_authenticity_token.inspect};".html_safe))
+        if protect_against_forgery?
+          (super + content_tag(:script, "this.Renderer || (this.Renderer = {}); Renderer.csrfToken = #{form_authenticity_token.inspect};".html_safe))
+        end
       end
     end
   end
